113fedbc478da0d551d2d63d9cf505b8ee3fa378
DianaBrightside <31443423+DianaBrightside@users.noreply.github.com>
2022-06-16 17:35:26 +0300

    final code cleaning

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,7 +2,6 @@ import React, { createContext, useReducer } from "react";
 
 import AppReducer from "./AppReducer";
 
-// Initial state
 const initialState = {
   transactions: [
     { id: 1, text: "Flower", amount: -20 },
@@ -12,10 +11,7 @@ const initialState = {
   ],
 };
 
-// Create context
 export const GlobalContext = createContext(initialState);
-
-// Provider component
 
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
--- src/styles/Buttons/AppButtons.js
+++ src/styles/Buttons/AppButtons.js
@@ -1,13 +1,7 @@
 import { Button } from "@mui/material";
 import styled from "styled-components";
 
-// export const DeleteHistoryCash = styled(Button)`
-//   padding: 0;
-//   min-width: 0;
-//   background: red;
-//   display: inline-block;
-// `;
-
 export const AddTransactionBtn = styled(Button)`
   width: 100%;
+  margin-bottom: 20px;
 `;
